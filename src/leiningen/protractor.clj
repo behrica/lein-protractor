585c78aefeed14260ca598d27f42ee162b0c0667
Carsten Behring <carsten.behring@gmail.com>
2013-11-10 19:05:03 +0100

    introduced some variables

--- src/leiningen/protractor.clj
+++ src/leiningen/protractor.clj
@@ -14,11 +14,11 @@
 (defn protractor
   "Starts/stop/runs selenium server, ring server, protractor"
   [project & args]
-  (System/setProperty "webdriver.chrome.driver" "/home/carsten/bin/chromedriver")
-  (let [selenium-server (org.openqa.selenium.server.SeleniumServer.)]
-    (.start selenium-server)
-    (future (server-task project {:join? false :port 8080 :open-browser? false :init (-> project :protractor :init )}))
-    (Thread/sleep 20000)
-    (println (sh "protractor" "resources/protractor_conf.js"))
-    (.stop selenium-server))
-)
+  (let [protractor (:protractor project)]
+    (System/setProperty "webdriver.chrome.driver" (:chromedriver protractor ))
+    (let [selenium-server (org.openqa.selenium.server.SeleniumServer.)]
+      (.start selenium-server)
+      (future (server-task project {:join? false :port 8080 :open-browser? false :init (:init protractor)}))
+      (Thread/sleep 20000)
+      (println (sh "protractor" (:protractorconfig protractor)))
+      (.stop selenium-server))))
