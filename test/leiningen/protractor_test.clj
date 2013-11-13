3640e503da2953a639303616f32ea6a79969fa64
Carsten Behring <carsten.behring@gmail.com>
2013-11-13 08:12:46 +0100

    removed working tests

--- test/leiningen/protractor_test.clj
+++ test/leiningen/protractor_test.clj
@@ -5,24 +5,6 @@
                [clojure.java.shell :refer [sh]]
                [leiningen.ring.server :refer [server-task]]))
 
-(defn deep-merge-with
-  "Like merge-with, but merges maps recursively, applying the given fn
-  only when there's a non-map at a particular level.
-
-  (deepmerge + {:a {:b {:c 1 :d {:x 1 :y 2}} :e 3} :f 4}
-               {:a {:b {:c 2 :d {:z 9} :z 3} :e 100}})
-  -> {:a {:b {:z 3, :c 3, :d {:z 9, :x 1, :y 2}}, :e 103}, :f 4}"
-  [f & maps]
-  (apply
-    (fn m [& maps]
-      (if (every? map? maps)
-        (apply merge-with m maps)
-        (apply f maps)))
-    maps))
-
-
-
-
 (defn noop-handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
@@ -40,35 +22,14 @@
 (def project { :ring {:handler noop-handler}
                :protractor protractor-config})
 
-(fact "when shell out to protactor call fails, abort() is called"
-    (protractor project) => nil
-    (provided
-      (abort "Protractor failed")  => nil
-      (sh "protractor" "a.js") => {}
-      (wait-a-bit) => nil))
-
-(fact "when shell out to protactor call works, abort() is not called"
-  (protractor project) => nil
-  (provided
-    (abort "Protractor failed")  => nil :times 0
-    (sh "protractor" "a.js") => {:exit 0}
-    (wait-a-bit) => nil))
-
-
-
-(fact "throws when called with misisng data"
-  (protractor {}) => (throws AssertionError)
-  (protractor {:protractor {}}) => (throws AssertionError))
-
 
 ; fails ...   see https://github.com/marick/Midje/issues/251
-
-;(fact "passing a port uses it"
-;  (protractor project) => nil
-;  (provided
-;    (start-ring project protractor-config) => nil
-;    (abort "Protractor failed")  => nil :times 0
-;    (wait-a-bit) => ""
-;    (sh-protractor protractor-config) => nil
-; ))
+(fact "passing a port uses it"
+  (protractor project) => nil
+  (provided
+    (start-ring project protractor-config) => nil
+    (abort "Protractor failed")  => nil :times 0
+    (wait-a-bit) => ""
+    (sh-protractor protractor-config) => nil
+))
 
